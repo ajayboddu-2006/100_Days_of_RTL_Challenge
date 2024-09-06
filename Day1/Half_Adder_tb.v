29cd576ee794396ef4090113f384952019d6caf7
Boddu Ajay <129162187+ajayboddu-2006@users.noreply.github.com>
2024-09-06 14:39:05 +0530

    Update Half_Adder_tb.v

--- Day1/Half_Adder_tb.v
+++ Day1/Half_Adder_tb.v
@@ -1,1 +1,18 @@
-
+`timescale 1ns/1ns
+module adder_test;
+    reg a, b;
+    wire sum, carry;
+    adder DUT(.a(a), .b(b), .sum(sum), .carry(carry));
+    initial begin
+        $dumpfile("adder.vcd");
+        $dumpvars(0, adder_test);
+        $monitor($time, "  a = %b, b = %b, sum = %b, carry = %b", a, b, sum, carry);
+        #5 a = 0; b = 0;  // 00 -> 0 sum, 0 carry
+        #5 a = 1; b = 0;  // 01 -> 1 sum, 0 carry
+        #5 a = 1; b = 1;  // 11 -> 0 sum, 1 carry
+        #5 a = 0; b = 1;  // 10 -> 1 sum, 0 carry
+        #5 a = 1; b = 1;  // 11 -> 0 sum, 1 carry
+        #5 a = 0; b = 0;  // 00 -> 0 sum, 0 carry
+        #5 $finish;
+    end
+endmodule
